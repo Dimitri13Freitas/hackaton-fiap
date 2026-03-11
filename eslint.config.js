c253b5b9075edfd3cabda84d36cb528271643e8c
Dimitri13Freitas <freitasdimitri7@gmail.com>
2026-03-10 22:19:01 -0300

    fix: ci/cd

--- eslint.config.js
+++ eslint.config.js
@@ -5,11 +5,14 @@ module.exports = {
     tsconfigRootDir: __dirname,
     project: ["./apps/*/tsconfig.json"],
   },
+  rules: {
+    "no-unused-vars": "warn",
+  },
   plugins: ["@typescript-eslint"],
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
-    "plugin:storybook/recommended"
+    "plugin:storybook/recommended",
   ],
 };
