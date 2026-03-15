2008cb7245b1e712348661d9c2a3ce5a741d5f65
Dimitri13Freitas <freitasdimitri7@gmail.com>
2026-03-15 10:06:59 -0300

    fix: bug on alias viteconfig

--- apps/mfe-login/vite.config.ts
+++ apps/mfe-login/vite.config.ts
@@ -26,11 +26,6 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      react: path.resolve(__dirname, "./node_modules/react"),
-      "react/jsx-runtime": path.resolve(
-        __dirname,
-        "./node_modules/react/jsx-runtime",
-      ),
       "@": path.resolve(__dirname, "./src"),
     },
   },
--- apps/mfe-settings/vite.config.ts
+++ apps/mfe-settings/vite.config.ts
@@ -25,11 +25,6 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      react: path.resolve(__dirname, "./node_modules/react"),
-      "react/jsx-runtime": path.resolve(
-        __dirname,
-        "./node_modules/react/jsx-runtime",
-      ),
       "@": path.resolve(__dirname, "./src"),
     },
   },
